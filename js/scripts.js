2533b72a78cf37715669f1b20cc37d45257db73b
Saguna Nathani <sagunanathani@gmail.com>
2025-06-10 17:05:50 +0200

    Add variables & alert

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//see a familiar pop-up
+alert('Hello Simple JS App');
+
+let myName = 'sagu k';
+document.write(myName);
+
+myName = 'sagu kathiriya';
+document.write(myName);
+
+let favoriteFood = 'pizza';
+document.writeln(favoriteFood);
