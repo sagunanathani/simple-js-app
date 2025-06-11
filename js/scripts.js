309e77e25db0f540289c0a000b7b03f6d453d927
Saguna Nathani <sagunanathani@gmail.com>
2025-06-11 14:47:55 +0200

    Create Array Of Object 'pokemonList'

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+let pokemonList = [
+    {name: "Bulbasaur", height: 6, types: ['grass', 'poison']},
+    {name: "Butterfree", height: 5, types: ['bug', 'fighting']},
+    {name: "Ekans", height: 6, types: ['water', 'fairy']},
+    {name: "Psyduck", height: 7, types: ['ice', 'ground']}
+];
+console.log(pokemonList);
