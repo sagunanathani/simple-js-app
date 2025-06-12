b59e336ddf158e5287d9de518303f5d1426ac407
Saguna Nathani <sagunanathani@gmail.com>
2025-06-12 20:38:57 +0200

    Display Pokémon list with heights and conditional highlight for tall Pokémon

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
+// Define an array of Pokémon objects
 let pokemonList = [
-    {name: "Bulbasaur", height: 6, types: ['grass', 'poison']},
-    {name: "Butterfree", height: 5, types: ['bug', 'fighting']},
-    {name: "Ekans", height: 6, types: ['water', 'fairy']},
-    {name: "Psyduck", height: 7, types: ['ice', 'ground']}
+    {name: "Bulbasaur", height: 1, types: ['grass', 'poison']},
+    {name: "Butterfree", height: 0.5, types: ['bug', 'fighting']},
+    {name: "Ekans", height: 3.5, types: ['water', 'fairy']},
+    {name: "Psyduck", height: 0.7, types: ['ice', 'ground']}
 ];
 console.log(pokemonList);
+
+// Loop through each Pokémon and write its name and height
+for (let i = 0; i < pokemonList.length; i++) {
+  console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+  document.writeln(pokemonList[i].name + " (height: " + pokemonList[i].height + ") <br>");
+} 
+
+// Highlight if height is greater than 3
+for (let i = 0; i < pokemonList.length; i++) {
+    if(pokemonList[i].height > 3){
+        console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that’s big!");
+    }
+} 
+
+
+
+
