c1fa9dfb10be21b82b5866e15a178d652c3fd00c
Saguna Nathani <sagunanathani@gmail.com>
2025-06-17 14:32:15 +0200

    added IIFE to the pokemon Array
    Make changes in script and added new variable PokemonRespository to hold an Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,35 @@
-// Define an array of Pokémon objects
-let pokemonList = [
-  // pokemon objects
-  { name: "Bulbasaur", height: 1, types: ["grass", "poison"] },
-  { name: "Butterfree", height: 0.5, types: ["bug", "fighting"] },
-  { name: "Ekans", height: 3.5, types: ["water", "fairy"] },
-  { name: "Psyduck", height: 0.7, types: ["ice", "ground"] },
-];
-console.log(pokemonList);
-
-//Display Pokémon list with name, height, and conditional message with same loop
-for (let i = 0; i < pokemonList.length; i++) {
-  let pokemon = pokemonList[i];
-  let output = pokemon.name + " (height: " + pokemon.height + ")";
-  if (pokemon.height > 3) {
-    output += " - Wow, that’s big!";
-  }
-  //console.log(output);
-  //document.writeln(output + "<br>");
-}
-
-//Display Pokémon list with forEach
-pokemonList.forEach(function (pokemon) {
-  let output = pokemon.name + " (height: " + pokemon.height + ")";
-  if (pokemon.height > 3) {
-    output += " - Wow, that’s big!";
-  }
-  //console.log(output);
-  document.writeln(output + "<br>");
+// create new variable pokemonRepository
+let pokemonRepository = (function () {
+  // Define an array of Pokémon objects
+  let pokemonList = [
+    // pokemon objects
+    { name: "Bulbasaur", height: 1, types: ["grass", "poison"] },
+    { name: "Butterfree", height: 0.5, types: ["bug", "fighting"] },
+    { name: "Ekans", height: 3.5, types: ["water", "fairy"] },
+    { name: "Psyduck", height: 0.7, types: ["ice", "ground"] },
+  ];
+  return {
+    add: function (pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function () {
+      return pokemonList;
+    },
+  };
+})();
+//console.log(pokemonList);
+console.log(pokemonRepository.getAll()); // [pokemonList]
+pokemonRepository.add({
+  name: "Pikachu",
+  height: 0.6,
+  types: ["bug", "poison"],
 });
+console.log(pokemonRepository.getAll()); // [ { name , height & types } ]
 
+//Display Pokémon list with forEach Loops
+pokemonRepository.getAll().forEach(function (pokemon) {
+  console.log(pokemon.name + " (height: " + pokemon.height + ")");
+  document.writeln(
+    "<p>" + pokemon.name + " (height: " + pokemon.height + ")</p>"
+  );
+});
